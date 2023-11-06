7abb756977d368da6ea53a5c99c7da26bce4c48a
MemanAwad <meman1awad@gmail.com>
2023-11-06 12:06:47 +0200

    return element at index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
-        print("{:d}".format(i))
+def element_at(my_list, idx):
+    listlen = len(my_list) - 1
+    if idx < 0 or idx > l:
+        return (None)
+    else:
+        return (my_list[idx])
