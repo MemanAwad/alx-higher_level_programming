460b80ca431c6196ddb28520f258522a80682c7b
MemanAwad <meman1awad@gmail.com>
2023-11-06 14:18:38 +0200

    print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             k += 1
             if a > k:
-                print("{} " .format(j), end=" ")
+                print("{:d} " .format(j), end=" ")
             else:
-                print("{}" .format(j), end="")
+                print("{:d}" .format(j), end="")
         print()
