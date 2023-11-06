c8e1810402c4002c6d23f3ad847c39200755023d
MemanAwad <meman1awad@gmail.com>
2023-11-06 16:16:30 +0200

    find max element

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     len_list = len(my_list)
-    maxx = 0
     if len_list == 0:
         return (None)
+    maxx = my_list[0]
     for i in range(len_list):
         if my_list[i] > maxx:
             maxx = my_list[i]
