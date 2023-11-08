6a4133af571deff355e76dc0d9dfcd644947676f
MemanAwad <meman1awad@gmail.com>
2023-11-09 00:19:50 +0200

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not len(a_dictionary) == 0:
-        return (None)
-
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
     maxx = list(a_dictionary.keys())[0]
     bkey = a_dictionary[maxx]
     for x, y in a_dictionary.items():
-        if y < bkey:
+        if y > bkey:
             bkey = y
             maxx = x
     return (maxx)
