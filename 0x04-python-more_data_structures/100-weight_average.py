307f22bba0d483a401c05f3aec41e1c110eee29e
MemanAwad <meman1awad@gmail.com>
2023-11-09 01:05:39 +0200

    average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     if not isinstance(my_list, list) or len(my_list) == 0:
-        return None
+        return 0
     summ, avg = 0, 0
     for x in my_list:
         summ += x[0] * x[1]
