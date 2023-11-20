affe23b10f5f9f02492a4afc7150c27b3dd7fe54
MemanAwad <meman1awad@gmail.com>
2023-11-20 23:05:11 +0200

    print exception error

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+        try:
+            print("{:d}".format(value))
+            return True
+        except TypeError as ty:
+            print("Exception:", ty.exe, file=sys.stderr)
+        except ValueError as va:
+            print("Exception:", va, file=sys.stderr)
+        return False
