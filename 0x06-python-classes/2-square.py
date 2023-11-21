63c050c805c8d0f49e6a38536b71d859228a2a67
MemanAwad <meman1awad@gmail.com>
2023-11-21 19:27:29 +0200

    handle errors

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+"""This module is going to initiate a python empty class"""
+
+
+class Square:
+    """ empty class Square that defines a square
+
+    Attributes:
+            size (int): hold the size of the square."""
+
+    __size = 0
+
+    def __init__(self, size=0):
+        """ initializer of the default size of the object
+
+        Args:
+            size (str): the size of the square."""
+
+        try:
+            if (size < 0):
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        except TypeError:
+            print("size must be an integer")
