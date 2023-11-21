a18e9b2913b292803f3f81df6d4fd8deb3238e7f
MemanAwad <meman1awad@gmail.com>
2023-11-21 19:04:15 +0200

    add size attribute to the class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,12 +4,17 @@
 
 
 class Square:
-    """ empty class Square that defines a square """
+    """ empty class Square that defines a square
+
+    Attributes:
+            size (int): hold the size of the square."""
 
     __size = None
 
     def __init__(self, size=None):
-        """ initializer of the default size of the object"""
+        """ initializer of the default size of the object
+
+        Args:
+            size (str): the size of the square."""
 
         self.__size = size
-
