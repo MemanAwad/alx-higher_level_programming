ddbd9123c2b0d6893f3498a4ee986371c8a4c224
MemanAwad <meman1awad@gmail.com>
2023-11-27 21:51:15 +0200

    add a Class method def square(cls, size=0): that returns a new Rectangle instance with width == height == size

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,8 +6,6 @@
 class Rectangle:
     """ this class is going to define a reactangle"""
 
-    __width = 0
-    __height = 0
     number_of_instances = 0
     print_symbol = "#"
 
@@ -45,23 +43,6 @@ class Rectangle:
         return strr
 
     @property
-    def height(self):
-        """ return the height of the rectangle"""
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ to set the value of the height """
-
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-
-    @property
     def width(self):
         """ return the width of the rectangle"""
 
@@ -71,12 +52,27 @@ class Rectangle:
     def width(self, value):
         """ to set the value of the width """
 
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
+
+    @property
+    def height(self):
+        """ return the height of the rectangle"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ to set the value of the height """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         """Return the area of the rectangle"""
