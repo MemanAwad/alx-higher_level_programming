d9ec8d0bb10988cf74fc0379af784a960726b33b
MemanAwad <meman1awad@gmail.com>
2023-11-27 21:45:18 +0200

    make a rectangle with width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,8 +8,24 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ the init method"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        """ return the width of the rectangle"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ to set the value of the width """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -26,19 +42,3 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    @property
-    def width(self):
-        """ return the width of the rectangle"""
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ to set the value of the width """
-
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
