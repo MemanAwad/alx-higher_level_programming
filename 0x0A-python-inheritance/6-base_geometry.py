9878874cc581319256de563003aa93f667149e4b
MemanAwad <meman1awad@gmail.com>
2023-12-04 18:11:13 +0200

    area method raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" BaseGeometry class"""
+
+
+class BaseGeometry:
+    """ Base class"""
+
+    def area(self):
+        """ method to raise an exeption"""
+
+        raise Exception("area() is not implemented")
