b86bc41b7aa3f666a14678cd05da22138c59ef69
MemanAwad <meman1awad@gmail.com>
2023-12-05 18:38:11 +0200

    load, add, and save to json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 """ json losd, add, and save"""
+import json
+import os.path
+import sys
 from sys import argv
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+listt = []
 
-argv.pop(0)
-try:
-    deserialized = load_from_json_file("add_item.json")
-    if deserialized is None:
-        save_to_json_file(argv, "add_item.json")
-    else:
-        deserialized.extend(argv)
-        save_to_json_file(argv, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(argv, "add_item.json")
+if os.path.exists("add_item.json"):
+    listt = load_from_json_file("add_item.json")
+
+for strr in argv[1:]:
+    listt.append(strr)
+
+save_to_json_file(listt, "add_item.json")
