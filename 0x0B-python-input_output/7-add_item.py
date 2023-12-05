0faab00a5b1ad7c076c2236c7e4e41ad3cb4af2b
MemanAwad <meman1awad@gmail.com>
2023-12-05 22:44:00 +0200

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """ json losd, add, and save"""
-import json
-import os.path
 import sys
-from sys import argv
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+if __name__ == "__main__":
+    load_from_json_file = __import__('6-load_from_json_file').\
+        load_from_json_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-listt = []
-
-if os.path.exists("add_item.json"):
-    listt = load_from_json_file("add_item.json")
-
-for strr in argv[1:]:
-    listt.append(strr)
-
-save_to_json_file(listt, "add_item.json")
+    try:
+        listt = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        listt = []
+    listt.extend(sys.argv[1:])
+    save_to_json_file(listt, "add_item.json")
