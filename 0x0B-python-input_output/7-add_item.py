b245c9b8611db4bc69b6ffaab45540558964ea83
MemanAwad <meman1awad@gmail.com>
2023-12-05 18:28:05 +0200

    load, add, and save json file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+
 argv.pop(0)
 try:
     deserialized = load_from_json_file("add_item.json")
