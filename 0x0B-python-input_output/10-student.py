79389f3e7c72116fdd358b771f2524337b9c01cf
MemanAwad <meman1awad@gmail.com>
2023-12-05 22:06:44 +0200

    add to student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" student class module"""
+
+
+class Student:
+    """student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """constructor"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance """
+
+        if attrs is None:
+            return self.__dict__
+
+        stu = {}
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                stu[key] = value
+
+        return stu
