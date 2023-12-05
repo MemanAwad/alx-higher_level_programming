3eab971cfc8cbcb5ad6245afffb112a7c1de0091
MemanAwad <meman1awad@gmail.com>
2023-12-05 16:58:27 +0200

    function to write in the file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" write ti a foole module"""
+
+
+def write_file(filename="", text=""):
+    """function to write in a file"""
+
+    with open(filename , "w") as f1:
+        return (f1.write(text))
+
