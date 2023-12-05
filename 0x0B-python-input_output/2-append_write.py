eddc8a0978a274ca45f79be615bb3ba1e1f797ad
MemanAwad <meman1awad@gmail.com>
2023-12-05 17:10:24 +0200

    append text to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,5 +5,5 @@
 def append_write(filename="", text=""):
     """ function to append text to the file content"""
 
-    with open(filename, "a") as f:
-        return(f.write(text))
+    with open(filename, "a", encoding="utf-8") as f:
+        return (f.write(text))
