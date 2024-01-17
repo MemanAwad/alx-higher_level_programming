e0962775813898c1c2e65f2820959d8ab1d82e75
MemanAwad <meman1awad@gmail.com>
2024-01-18 01:21:44 +0200

    citizes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,12 @@
 -- Create database hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Create table if it does not exist
-CREATE TABLE IF NOT EXISTS cities (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+-- Create table cities if it does not exist
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+	PRIMARY KEY(`id`),
+	`id`       INT          NOT NULL AUTO_INCREMENT,
+	`state_id` INT          NOT NULL,
+	`name`     VARCHAR(256) NOT NULL,
+	FOREIGN KEY(`state_id`)
+	REFERENCES `hbtn_0d_usa`.`states`(`id`)
 );
