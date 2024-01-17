ee692dcc6e9aac19164cb750ce78c25ad8c416b1
MemanAwad <meman1awad@gmail.com>
2024-01-18 01:17:35 +0200

    primary key

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,8 +1,8 @@
 -- Create hbtn_0d_usa if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Create table states if it does not exist
-CREATE TABLE IF NOT EXISTS  'hbtn_0d_usa'.'states' (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRIMARY KEY(`id`),
+	`id`   INT          NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL
 );
-
