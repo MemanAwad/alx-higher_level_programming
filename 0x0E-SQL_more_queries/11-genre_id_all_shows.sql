64e97e84bec111f7d319e9028b2d91ccddedbfc6
MemanAwad <meman1awad@gmail.com>
2024-01-18 01:37:20 +0200

    genre_id_all_shows

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,6 +1,6 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows.
 SELECT s.`title`, g.`genre_id`
-FROM `tv_shows` AS sh
-	LEFT JOIN `tv_show_genres` AS gen
-	ON sh.`id` = gen.`show_id`
-ORDER BY sh.`title`, gen.`genre_id`;
+	FROM `tv_shows` AS sh
+		LEFT JOIN `tv_show_genres` AS gen
+		ON sh.`id` = gen.`show_id`
+	ORDER BY sh.`title`, gen.`genre_id`;
