07f8699229a547b9a6735e54d55e1b247232b5cb
MemanAwad <meman1awad@gmail.com>
2024-01-18 01:30:55 +0200

    list cities and states

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- List all cities with their respective state names
-SELECT cities.id, cities.name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
