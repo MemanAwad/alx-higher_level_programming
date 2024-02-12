b30db589a9189eb54261d0da621681c39be0bd1a
MemanAwad <meman1awad@gmail.com>
2024-02-13 00:29:00 +0200

    more work with argument

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv.length > 2)
+{
+	for (let i = 2; i < process.argv.length; i++)
+	{
+		console.log(process.argv[i])
+	}
+}
+else
+{
+	console.log("No argument")
+}
+
