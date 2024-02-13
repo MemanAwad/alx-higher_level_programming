203abdb207a66b9af7eeb32a08fa6fa9efed21ba
MemanAwad <meman1awad@gmail.com>
2024-02-13 02:19:50 +0200

    print x times

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let myVar = process.argv[2];
+let myVar2 = parseInt(myVar);
+if (!isNaN(myVar2))
+{
+	for (let i = 0; i < myVar2; i++)
+	{
+		console.log('C is fun');
+	}
+}
+else
+{
+	console.log('Missing number of occurrences');
+}
