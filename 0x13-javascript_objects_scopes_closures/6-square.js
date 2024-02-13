005d73d5656a93614f36830d2ad73a7f9293414d
MemanAwad <meman1awad@gmail.com>
2024-02-14 00:04:04 +0200

    add new function to square that inherts from square5

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Squaree = require('./5-square');
+
+class Square extends Squaree {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write(c);
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Square;
