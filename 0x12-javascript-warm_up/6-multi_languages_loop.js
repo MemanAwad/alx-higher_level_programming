396d2b4f9c575da72f1aa56b3976a7a3f03ff134
MemanAwad <meman1awad@gmail.com>
2024-02-13 04:08:37 +0200

    create an array

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
-  console.log(myVar[i]);
+for (const i of myVar) {
+  console.log(i);
 }
