3e74541a8eb793c891e80930b035ae38f758e7ab
MemanAwad <meman1awad@gmail.com>
2024-02-13 03:52:09 +0200

    add 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-const myVar1 = process.argv[2];
-const myVar2 = parseInt(myVar1);
-const myVar3 = process.argv[3];
-const myVar4 = parseInt(myVar3);
-
-if ((!isNaN(myVar2)) && (!isNaN(myVar4))) {
-  console.log(myVar2 + myVar4);
-} else {
-  console.log('NaN');
+function add (a, b) {
+  return a + b;
 }
+console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
