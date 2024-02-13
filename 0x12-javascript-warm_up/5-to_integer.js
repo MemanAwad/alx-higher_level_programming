cbf955a1ce4a1c874304a197d725e8717f944bd4
MemanAwad <meman1awad@gmail.com>
2024-02-13 02:22:47 +0200

    fix semitandard

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
-let myVar = process.argv[2];
-let myVar2 = parseInt(myVar);
-if (!isNaN(myVar2))
-{
-	console.log('My number:', myVar2)
-}
-else
-{
-	console.log('Not a number')
+const myVar = process.argv[2];
+const myVar2 = parseInt(myVar);
+if (!isNaN(myVar2)) {
+  console.log('My number:', myVar2);
+} else {
+  console.log('Not a number');
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-let myVar = process.argv[2];
-let myVar2 = parseInt(myVar);
-if (!isNaN(myVar2))
-{
-	for (let i = 0; i < myVar2; i++)
-	{
-		console.log('C is fun');
-	}
-}
-else
-{
-	console.log('Missing number of occurrences');
+const myVar = process.argv[2];
+const myVar2 = parseInt(myVar);
+if (!isNaN(myVar2)) {
+  for (let i = 0; i < myVar2; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
