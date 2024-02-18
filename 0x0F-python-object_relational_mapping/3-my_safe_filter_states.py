2613c96f251803ff532d64861cf7dabe3561e745
MemanAwad <meman1awad@gmail.com>
2024-02-18 19:43:44 +0200

    safe from injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -18,8 +18,7 @@ if __name__ == "__main__":
     cur.execute(sql_query, (sys.argn[4]))
     result = cur.fetchall()
     for row in result:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
 
     cur.close()
     db.close()
