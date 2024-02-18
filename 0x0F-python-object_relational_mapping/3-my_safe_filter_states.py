4274aa78259f93553c0317da74485ae4e2f8a163
MemanAwad <meman1awad@gmail.com>
2024-02-18 19:31:39 +0200

    safe from injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
             db=sys.argv[3]
     )
     cur = db.cursor()
-    sql_query = "SELECT * FROM states WHERE name=%s ORDER BY id ASC"
-    cur.execute(sql_query, (sys.argn[4], ))
+    sql_query = "SELECT * FROM states WHERE name=%s"
+    cur.execute(sql_query, (sys.argn[4]))
     result = cur.fetchall()
     for row in result:
         if row[1] == sys.argv[4]:
