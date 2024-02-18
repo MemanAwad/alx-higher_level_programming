9c7d46e43a363275eb7b1030c08c47653fb1430d
MemanAwad <meman1awad@gmail.com>
2024-02-19 00:27:54 +0200

    lists all State objects from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" script to list all the states objects from datadase"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+                sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id).all()
+
+    for row in query:
+        print("{}:{}".format(row.id, row.name))
+
+    session.close()
