1037a6113e287fd26bf7dc9e688bdff44c014033
MemanAwad <meman1awad@gmail.com>
2024-02-18 18:39:28 +0200

    show all the states starts with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" list all states with name staring with N"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    sql_query = "SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC"
+    cur.execute(sql_query)
+    result = cur.fetchall()
+    for row in result:
+        if row[1][0] == 'N': 
+            print(row)
+
+    cur.close()
+    db.close()
